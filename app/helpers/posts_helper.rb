73a513030f21e76e5d559c7b62453320a8fd6421
Chris Aporta <christopher.aporta@gmail.com>
2016-03-06 22:28:38 -0500

    complete lab

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,9 @@
 module PostsHelper
-  
+  def author_id_field(post,f,authors)
+    if post.author.nil?
+      f.collection_select :author_id, authors, :id, :name
+    else
+      f.hidden_field :author_id
+    end
+  end
 end
