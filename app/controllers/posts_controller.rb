03478996c79a03a82c7ad3864ef5e718466388dd
Chris Aporta <christopher.aporta@flatironschool.com>
2016-03-07 18:36:59 -0500

    fix songs_controller redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
         redirect_to authors_path, alert: "Author not found."
       else
         @post = author.posts.find_by(id: params[:id])
-        redirect_to author_post_path(author), alert: "Post not found." if @post.nil?
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
       end
     else
       @post = Post.find(params[:id])
