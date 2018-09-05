1259f35bfd35a9c8f0aae080b0cb2244834e0efc
ElvinGarcia <dimix181@gmail.com>
2018-09-05 14:21:52 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
   resources :authors, only: [:show, :index] do
-    resources :posts, only: [:show, :index]
-  end
-
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
-  root 'posts#index'
+  resources :posts, only: [:show, :index, :new, :edit]
+end
+  resources :posts
 end
