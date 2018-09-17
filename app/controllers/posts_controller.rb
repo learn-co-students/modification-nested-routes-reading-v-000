e649cd27c41b9013c1e4fcd7c5fe543b26aa5584
Richard-Frost <frost.rchrd@gmail.com>
2018-09-17 13:30:39 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,18 +33,18 @@ class PostsController < ApplicationController
   end
 
   def edit
-  if params[:author_id]
-    author = Author.find_by(id: params[:author_id])
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id]) #finds the author based off of params[:author_id]
     if author.nil?
       redirect_to authors_path, alert: "Author not found."
     else
-      @post = author.posts.find_by(id: params[:id])
+      @post = author.posts.find_by(id: params[:id]) #finds the post through the authour to make sure its the correct post
       redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
     end
-  else
-    @post = Post.find(params[:id])
+    else
+      @post = Post.find(params[:id])
+    end
   end
-end
 
   private
 
