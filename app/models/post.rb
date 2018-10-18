6eaeda5316d9a4a8dc4200a5c5bc544ba5a93bc9
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-10-18 23:52:02 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
   #put new code here
@@ -20,7 +20,7 @@ class Post < ActiveRecord::Base
   private
 
   def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
+    if title.split.any?{|w| w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
     end
   end
