e59591ac0d6c812ab15ec0fabac6e38c0b094a15
nichia <nichia@gmail.com>
2018-10-28 15:05:38 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   resources :authors, only: [:show, :index] do
-    resources :posts, only: [:show, :index]
+    resources :posts, only: [:show, :index, :new, :edit]
   end
+#   Prefix Verb  URI Pattern                             Controller#Action
+# author_posts GET   /authors/:author_id/posts(.:format)     posts#index
+# new_author_post GET   /authors/:author_id/posts/new(.:format) posts#new
+# edit_author_post GET   /authors/:author_id/posts/:id/edit(.:format) posts#edit
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
