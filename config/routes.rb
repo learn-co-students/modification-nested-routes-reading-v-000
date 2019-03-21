32e882f5c16503f9401f0047f65115e46ca14593
RonSala <ron.sala@gmail.com>
2019-03-21 10:01:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :edit]
   end
+
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  root 'posts#index'
 end
