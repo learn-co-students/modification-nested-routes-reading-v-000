18b442456949245cbeabf0dc7c9f6aa81525f161
Leslie Stevens <flymaven@protonmail.com>
2019-04-15 15:21:25 -0500

    edit controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
+  end
+
+  resources :authors, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
