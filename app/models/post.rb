14754c89a5b482e40c17392d9ed6156907cde260
Ashley <ashleywong678@gmail.com>
2019-04-21 13:46:09 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,6 +16,14 @@ class Post < ActiveRecord::Base
   def self.by_author(author_id)
     where(author: author_id)
   end
+  
+  def author_id_field(post)
+    if post.author.nil?
+      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+    else
+      hidden_field_tag "post[author_id]", post.author_id
+    end
+  end
 
   private
 
