faf740b18173b14e8801ca2e78c3d4c3a7ce1922
Liz P <parisielizabeth@gmail.com>
2019-12-10 04:07:08 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,20 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else 
+      @post = Post.new(author_id: params[:author_id])
+    end 
   end
+  
+  def new
+  if params[:author_id] && !Author.exists?(params[:author_id])
+    redirect_to authors_path, alert: "Author not found."
+  else
+    @post = Post.new(author_id: params[:author_id])
+  end
+end
 
   def create
     @post = Post.new(post_params)
@@ -32,13 +44,26 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+    
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    
+    else
+      @post = Post.find(params[:id])
   end
+end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
