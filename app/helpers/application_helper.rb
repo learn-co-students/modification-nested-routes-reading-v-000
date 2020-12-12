9c0ad94e87b1aef42913d3cb09e7da9f8eecd66b
github username <d24dust@hotmail.com>
2020-12-12 12:07:30 -0500

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def load_assets_for_controller(controller, action: nil)
+    if action
+      stylesheet_link_tag("#{controller}/#{action}")
+    else
+      stylesheet_link_tag("#{controller}")
+    end
+  end
 end
